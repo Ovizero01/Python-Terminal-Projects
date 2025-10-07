fc1c41965bb78616eba027e05d8b9e9a6bead160
Rakib Ahmed Ovi <ahmed8050520@gmail.com>
2025-10-07 14:28:33 +0600

    Ride Sharing

---
+++ Ride Sharing/main.py
@@ -0,0 +1,11 @@
+from ride import RideSharing
+from users import Rider, Driver
+
+niye_jao = RideSharing("Niye Jao")
+rahim = Rider("Rahim Uddin", "rahim@gmail.com", 1234, "Mohakhali", 1200)
+niye_jao.add_rider(rahim)
+kolimuddin = Driver("Kolim Uddin", "kolim@gmail.com", 2345, "Gulshan")
+niye_jao.add_driver(kolimuddin)
+rahim.request_ride(niye_jao, "Uttara", "car")
+kolimuddin.reach_destination(rahim.current_ride)
+rahim.show_current_ride()
